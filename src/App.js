f64a9c556a9070e86d0c4744e1cb7cebc915ed97
rahmor <rahmor@yahoo.com>
2019-04-18 15:48:28 -0400

    add handlers;

--- src/App.js
+++ src/App.js
@@ -1,31 +1,71 @@
 import React, { Component } from 'react';
-import List from './List'
+import List from './List';
 import './App.css';
 
 class App extends Component {
-  static defaultProps = {
-    store: {
-      lists: [],
-      allCards: {},
-    }
+  constructor(props) {
+    super(props);
+    const { lists, allCards } = props.store;
+    this.state = { lists, allCards };
+  }
+
+  buildList = () => {
+    return this.state.lists.map(list => (
+      <List
+        key={list.id}
+        header={list.header}
+        cards={list.cardIds.map(id => this.state.allCards[id])}
+        onDeleteItem={this.handleDeleteItem}
+        onAddItem={this.handleAddItem}
+      />
+    ));
   };
 
+  handleDeleteItem = (header, id) => {
+    console.log('Delete Item', header, id);
+    //list card from lists array
+    //delete card from allcards array
+  };
+
+  handleAddItem = header => {
+    console.log('Add Item', header);
+    let newCard = this.newRandomCard();
+    //create random object
+    console.log(newCard);
+
+    //add to allCards array.
+  };
+
+  newRandomCard = () => {
+    const id =
+      Math.random()
+        .toString(36)
+        .substring(2, 4) +
+      Math.random()
+        .toString(36)
+        .substring(2, 4);
+    return {
+      id,
+      title: `Random Card ${id}`,
+      content: 'lorem ipsum'
+    };
+  };
+
+  omit(obj, keyToOmit) {
+    return Object.entries(obj).reduce(
+      (newObj, [key, value]) =>
+        key === keyToOmit ? newObj : { ...newObj, [key]: value },
+      {}
+    );
+  }
+
   render() {
-    const { store } = this.props
     return (
       <main className='App'>
         <header className='App-header'>
           <h1>Trelloyes!</h1>
         </header>
-        <div className='App-list'>
-          {store.lists.map(list => (
-            <List
-              key={list.id}
-              header={list.header}
-              cards={list.cardIds.map(id => store.allCards[id])}
-            />
-          ))}
-        </div>
+        <div className='App-list'>{this.buildList()}</div>
       </main>
     );
   }
--- src/List.js
+++ src/List.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Card from './Card'
+import Card from './Card';
 import './List.css';
 
 export default function List(props) {
@@ -9,20 +9,24 @@ export default function List(props) {
         <h2>{props.header}</h2>
       </header>
       <div className='List-cards'>
-        {props.cards.map((card) =>
+        {props.cards.map(card => (
           <Card
             key={card.id}
             title={card.title}
             content={card.content}
+            header={props.header}
+            id={card.id}
+            onDeleteItem={props.onDeleteItem}
           />
-        )}
+        ))}
         <button
           type='button'
           className='List-add-button'
+          onClick={() => props.onAddItem(props.header)}
         >
           + Add Random Card
         </button>
       </div>
     </section>
-  )
+  );
 }
